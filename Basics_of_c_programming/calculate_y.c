23f51c40de91dc4d89a48e922df09dc61e7318ba
Poojan Maharjan <pyunitoffice@gmail.com>
2026-01-06 20:41:44 +0545

    Update calculate_y.c

---
+++ Basics_of_c_programming/calculate_y.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float x, y;
+    printf("Enter value of x: ");
+    scanf("%f", &x);
+    y = 2*x + 5;  // Example formula, you can change as per your book
+    printf("y = %.2f\n", y);
+    return 0;
+}
